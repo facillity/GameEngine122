ebbf2297879518627c8e464786c4f8d3d0ccff88
facillity <ignacia@uci.edu>
2019-03-11 20:19:46 -0700

    Constructors, Methods for Board, GameState, Player classes

--- src/main/java/inf122/savage/engine/Board.java
+++ src/main/java/inf122/savage/engine/Board.java
@@ -1,5 +1,46 @@
 package inf122.savage.engine;
 
 public class Board{
-	
+	private int[][] boardSpace;
+	private int numRows;
+	private int numCols;
+
+	public Board(int userNumRows, int userNumCols){
+	    if (userNumRows <= 0 || userNumCols <= 0){
+	        // TO-DO: Handle 0 & Negative rows. Return Null?
+        }
+	    this.numRows = userNumRows;
+	    this.numCols = userNumCols;
+	    boardSpace = new int[userNumRows][userNumCols];
+	    zeroOutBoard();
+    }
+
+    public boolean zeroOutBoard(){
+	    for (int x = 0; x < numRows; x++){
+	        for (int y = 0; y < numCols; y++){
+	            boardSpace[x][y] = 0;
+            }
+        }
+	    return true;
+    }
+
+    public boolean change(int userNumRow, int userNumCol, int newTile){
+	    if (userNumRow < 0 || userNumRow >= this.numRows){
+	        return false;
+        } else if (userNumCol < 0 || userNumCol >= this.numCols){
+	        return false;
+        }
+	    //TO-DO: Add logic for newTile being something we don't want? Overkill?
+	    this.boardSpace[userNumRow][userNumCol] = newTile;
+	    return true;
+    }
+
+    public int getTile(int userNumRow, int userNumCol){
+        if (userNumRow < 0 || userNumRow >= this.numRows){
+            return -1;
+        } else if (userNumCol < 0 || userNumCol >= this.numCols){
+            return -1;
+        }
+        return this.boardSpace[userNumRow][userNumCol];
+    }
 }
--- src/main/java/inf122/savage/engine/Player.java
+++ src/main/java/inf122/savage/engine/Player.java
@@ -1,5 +1,61 @@
 package inf122.savage.engine;
 
 public class Player{
+    private String name;
+    private double score;
+    private int winCount;
 
+    // Constructor
+    public Player(){
+        this.name = "NO_NAME"; // Might want to change to Player1 / Player2 if we can keep track somehow..
+        this.score = 0;
+        this.winCount = 0;
+    }
+
+    public Player(String newName){
+        this.name = newName;
+        this.score = 0;
+        this.winCount = 0;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public double getScore(){
+        return this.score;
+    }
+
+    public int getWinCount(){
+        return this.winCount;
+    }
+
+    public boolean addWin(){
+        this.winCount++;
+        return true;
+    }
+
+    public boolean addScore(double points){
+        if (points < 0){
+            return false;
+        }
+        this.score = this.score + points;
+        return true;
+    }
+
+    public boolean subtractScore(double points){
+        if (points < 0){
+            return false;
+        }
+        this.score = this.score - points;
+        return false;
+    }
+
+    public boolean setName(String newName){
+        if (newName.length() == 0){
+            return false;
+        }
+        this.name = newName;
+        return true;
+    }
 }
