4669907db7f23fd4efb5c857ad73c8d0994fa308
Garik Khachatryan <gvkhacha@uci.edu>
2019-03-11 20:49:22 -0700

    json load/save util

---
+++ src/main/java/inf122/savage/engine/Board.java
@@ -0,0 +1,11 @@
+package inf122.savage.engine;
+
+/**
+  * Board is a wrapper for a static-sized two-dimensional 
+  * list of primitive types for a grid board game.
+  */
+public class Board{
+	Board(int numRows, int numCols){
+		
+	}
+}
