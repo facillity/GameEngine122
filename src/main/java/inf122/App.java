f52947f9354d0ba9323d9d6d5f0fbf2f1bf3e66b
Jacqueline D <jacdagdigian@gmail.com>
2019-03-12 18:20:57 -0700

    init fxml loader

--- src/main/java/inf122/App.java
+++ src/main/java/inf122/App.java
@@ -1,13 +1,28 @@
 package inf122;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends Application
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
+        stage.setTitle("");
+        stage.setScene(new Scene(root, 300, 285));
+        stage.show();
+
+    }
+
+
+    public static void main( String[] args ){
+        launch(args);
     }
 }
