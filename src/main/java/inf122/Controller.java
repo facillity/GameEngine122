635ec8c9f812ad96c6e0d20477be8951fc0ac4bb
Jacqueline D <jacdagdigian@gmail.com>
2019-03-14 14:40:59 -0700

    Initial GUI screen with four buttons

--- src/main/java/inf122/Controller.java
+++ src/main/java/inf122/Controller.java
@@ -1,4 +1,23 @@
 package inf122;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class Controller {
+
+
+    @FXML
+    void ff614a(ActionEvent event) {
+
+    }
+
+    @FXML
+    void launchGame(ActionEvent event) {
+        String gameName = event.getTarget().toString().substring(event.getTarget().toString().indexOf("'"));
+        System.out.println(gameName);
+
+    }
+
+
+
 }
