7edfda5a26b57a3d7a8a5ac03f9fddde06b8651f
Garik Khachatryan <gvkhacha@uci.edu>
2019-03-14 17:52:17 -0700

    gui refactor in package

---
+++ src/main/java/inf122/gui/Savage.java
@@ -0,0 +1,37 @@
+package inf122.gui;
+
+import inf122.savage.plugins.GamePlugin;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Savage extends Application {
+
+    @Override
+    public void init() throws Exception {
+        System.out.println("Hello World");
+    }
+
+
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("InitView.fxml"));
+        Parent root = loader.load();
+        SavageController controller = loader.getController();
+
+        for(GamePlugin g : GameRegistry.getGames()){
+            controller.addGame(g);
+        }
+
+
+        primaryStage.setTitle("");
+        primaryStage.setScene(new Scene(root, 300, 285));
+        primaryStage.show();
+    }
+}
